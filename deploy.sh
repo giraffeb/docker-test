191d09a3bf7237f8cb274abf142bf52e4b538c2f
giraffeb <rwpakr07@gmail.com>
2019-10-29 21:52:09 +0900

    찐막이길 바랍니다

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 
 sudo chmod +rwx ./docker-test.tar
 sudo docker load < ./docker-test.tar
-sudo docker run -d -p 8080:8080 giraffeb/docker-test
+sudo docker run -d -p 8080:9999 giraffeb/docker-test #포트번호는 변경가능합니다.
